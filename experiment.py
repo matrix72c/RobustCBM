c1f3d00d022ed544afa7bf6bf347e4bd30a117ed
matrix72 <matrix72c@gmail.com>
2024-03-21 00:05:58 +0800

    fix

--- experiment.py
+++ experiment.py
@@ -7,6 +7,8 @@ f = open("train.yaml", "r", encoding="utf-8")
 conf = yaml.load(f.read(), Loader=yaml.FullLoader)
 for exp in exps:
     conf["trainer"] = exp["trainer"]
+    conf["use_adv"] = ""
+    conf["use_noise"] = ""
     if "use_adv" in exp:
         for use_adv in exp["use_adv"]:
             conf["use_adv"] = use_adv
