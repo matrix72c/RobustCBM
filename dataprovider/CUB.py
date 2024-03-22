5250da6ea14fb891a6065e2a7def43f0bab9f017
matrix72 <matrix72c@gmail.com>
2024-03-22 21:58:54 +0800

    fix test data bug

--- dataprovider/CUB.py
+++ dataprovider/CUB.py
@@ -36,7 +36,7 @@ class CUB(Dataset):
             self.transform = transforms.Compose(
                 [
                     transforms.Resize((resol, resol)),
-                    transforms.CenterCrop(resol),
+                    # transforms.CenterCrop(resol),
                     transforms.ToTensor(),
                     transforms.Normalize(
                         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
