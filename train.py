d83834e0e931d56f419b1811acd937e0e8ea7e6d
matrix72 <matrix72c@gmail.com>
2024-03-24 02:38:10 +0800

    change backbone line

--- train.py
+++ train.py
@@ -201,7 +201,7 @@ def train(conf):
                     attr_acc_meter.avg,
                 )
             )
-            if label_acc_meter.avg > 0.80 and attr_acc_meter.avg > 0.90:
+            if label_acc_meter.avg > 0.80 and attr_acc_meter.avg > 0.85:
                 torch.save(
                     model.state_dict(),
                     "checkpoints/"
@@ -211,7 +211,7 @@ def train(conf):
                     + run_hash
                     + ".pth",
                 )
-            if label_acc_meter.avg > 0.95 and attr_acc_meter.avg > 0.90:
+            if label_acc_meter.avg > 0.95 and attr_acc_meter.avg > 0.85:
                 models = [f for f in os.listdir("checkpoints/") if run_hash in f]
                 min_diff = float("inf")
                 file_to_keep = None
