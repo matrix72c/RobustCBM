1bfb7219888233809499062b1f1795a212df0220
matrix72 <matrix72c@gmail.com>
2024-08-08 15:35:05 +0800

    fix transform

--- dataset/CUB.py
+++ dataset/CUB.py
@@ -33,7 +33,7 @@ class CUBDataSet(Dataset):
             self.data.extend(pickle.load(open(data_path + "val.pkl", "rb")))
             self.transform = transforms.Compose(
                 [
-                    transforms.Resize(size=(224, 224)),
+                    transforms.CenterCrop(224),
                     transforms.ToTensor(),
                     transforms.Normalize(
                         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
