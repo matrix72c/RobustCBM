ef374083d2bd72ed8b63a80e6e1e76c4b6711c60
matrix72 <matrix72c@gmail.com>
2025-01-09 21:52:59 +0800

    add gate

--- model/RCBM.py
+++ model/RCBM.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 import torch.nn.functional as F
 from model import CBM, VIB
 from VQ import VectorQuantizeEMA
@@ -10,6 +11,7 @@ class RCBM(CBM):
     def __init__(
         self,
         vib_lambda: float = 0.1,
+        use_gate: bool = True,
         embedding_dim: int = 64,
         codebook_size: int = 5120,
         codebook_weight: float = 0.25,
@@ -27,9 +29,9 @@ class RCBM(CBM):
             embedding_dim * self.num_concepts, self.num_concepts
         )
 
-        self.classifier = nn.Linear(
-            self.num_concepts, self.num_classes
-        ).apply(initialize_weights)
+        self.classifier = nn.Linear(self.num_concepts, self.num_classes).apply(
+            initialize_weights
+        )
 
         if quantizer == "EMA":
             self.quantizer = VectorQuantizeEMA(embedding_dim, codebook_size)
@@ -43,13 +45,16 @@ class RCBM(CBM):
         concept_pred, info_loss = self.concept_prob_gen(
             quantized_concept.view(quantized_concept.size(0), -1)
         )
-        label_pred = self.classifier(concept_pred)
+        c = torch.sigmoid(concept_pred) * 2.0 if self.hparams.use_gate else 1.0
+        label_pred = self.classifier(concept_pred * c)
         return label_pred, concept_pred, codebook_loss, info_loss
 
     def train_step(self, img, label, concepts):
         label_pred, concept_pred, codebook_loss, info_loss = self(img)
         label_loss = F.cross_entropy(label_pred, label)
-        concept_loss=F.binary_cross_entropy_with_logits(concept_pred, concepts, weight=self.dm.imbalance_weights.to(self.device))
+        concept_loss = F.binary_cross_entropy_with_logits(
+            concept_pred, concepts, weight=self.dm.imbalance_weights.to(self.device)
+        )
         loss = (
             label_loss
             + self.hparams.concept_weight * concept_loss
