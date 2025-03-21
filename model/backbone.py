549b26514cc19bf713fa905ed99b0549d78d1f4c
matrix72 <matrix72c@gmail.com>
2025-03-21 12:10:23 +0800

    fix backbone

--- model/backbone.py
+++ model/backbone.py
@@ -1,21 +1,16 @@
 import torch
 from torch import nn
 from model import CBM
-from utils import initialize_weights
+from utils import modify_fc
 
 
 class backbone(CBM):
     def __init__(self, **kwargs):
         kwargs["concept_weight"] = 0
         super().__init__(**kwargs)
-        in_features = self.base.fc.in_features
-        self.base = nn.Sequential(*list(self.base.children())[:-1])
-        self.classifier = nn.Linear(in_features, self.num_classes).apply(
-            initialize_weights
-        )
+        modify_fc(self.base, kwargs["base"], self.num_classes)
+        del self.classifier
 
     def forward(self, x):
         x = self.base(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x, torch.zeros(x.shape[0], self.num_concepts).to(x.device)
+        return x, torch.zeros(x.shape[0], self.num_concepts).detach().to(x.device)
