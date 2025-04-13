84ea5d4c10dcf397e3a6dd65b9c928644d23908c
matrix72 <matrix72c@gmail.com>
2025-04-13 12:55:52 +0800

    change cem

--- model/CEM.py
+++ model/CEM.py
@@ -10,24 +10,23 @@ class CEM(CBM):
         super().__init__(**kwargs)
         modify_fc(self.base, kwargs["base"], 2 * embed_dim * self.num_concepts)
 
-        self.concept_prob_gen = nn.Linear(
-            2 * embed_dim * self.num_concepts, self.num_concepts
-        ).apply(initialize_weights)
+        self.concept_prob_gen = nn.Linear(2 * embed_dim, 1).apply(initialize_weights)
 
         self.classifier = nn.Linear(
             embed_dim * self.num_concepts, self.num_classes
         ).apply(initialize_weights)
 
     def forward(self, x, concept_pred=None):
-        concept_context = self.base(x)
+        concept_context = self.base(x).reshape(x.size(0), -1, 2 * self.hparams.embed_dim)
         if concept_pred is None:
-            concept_pred = self.concept_prob_gen(concept_context)
-
+            concept_pred = (
+                self.concept_prob_gen(
+                    concept_context
+                )
+                .squeeze(-1)
+            )
         concept_probs = torch.sigmoid(concept_pred)
-        pos_embed, neg_embed = torch.chunk(concept_context, 2, dim=1)
-        pos_embed, neg_embed = pos_embed.view(
-            pos_embed.size(0), -1, self.hparams.embed_dim
-        ), neg_embed.view(neg_embed.size(0), -1, self.hparams.embed_dim)
+        pos_embed, neg_embed = concept_context.chunk(2, dim=2)
         concept_probs = concept_probs.unsqueeze(-1)
         combined_embed = pos_embed * concept_probs + neg_embed * (1 - concept_probs)
         concept_embed = combined_embed.view(combined_embed.size(0), -1)
