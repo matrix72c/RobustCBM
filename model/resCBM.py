f50c399f33d60e7d7ddc54c371706eae805e8db7
matrix72 <matrix72c@gmail.com>
2025-04-13 23:24:19 +0800

    refactor rescbm

--- model/resCBM.py
+++ model/resCBM.py
@@ -5,15 +5,19 @@ from utils import initialize_weights
 
 
 class resCBM(CBM):
-    def __init__(self, **kwargs):
+    def __init__(self, res_dim: int = 16, **kwargs):
         super().__init__(**kwargs)
         self.base.fc = nn.Linear(
-            self.base.fc.in_features, self.num_concepts + self.num_classes
+            self.base.fc.in_features, self.num_concepts + res_dim
+        ).apply(initialize_weights)
+        self.classifier = nn.Linear(
+            self.num_concepts + res_dim, self.num_classes
         ).apply(initialize_weights)
 
-    def forward(self, x):
+    def forward(self, x, concept_pred=None):
         logits = self.base(x)
-        concept_pred = logits[:, : self.num_concepts]
+        if concept_pred is None:
+            concept_pred = logits[:, : self.num_concepts]
         label_res = logits[:, self.num_concepts :]
-        label_pred = self.classifier(concept_pred) + label_res
+        label_pred = self.classifier(torch.cat([concept_pred, label_res], dim=1))
         return label_pred, concept_pred
